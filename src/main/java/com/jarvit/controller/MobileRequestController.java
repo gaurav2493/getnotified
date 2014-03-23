cb02d327b5f7c316b3ec37a1d385f4b22fb992c4
gaurav <gaurav2493@gmail.com>
2014-03-23 16:44:48 +0530

    doneee

--- src/main/java/com/jarvit/controller/MobileRequestController.java
+++ src/main/java/com/jarvit/controller/MobileRequestController.java
@@ -10,12 +10,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jarvit.helper.Album;
 import com.jarvit.helper.URLHtml;
+import com.shephertz.app42.paas.sdk.java.App42API;
+import com.shephertz.app42.paas.sdk.java.push.PushNotification;
+import com.shephertz.app42.paas.sdk.java.push.PushNotificationService;
 
 @Controller
 public class MobileRequestController {
 
 	@Autowired
 	DataSource dataSource;
+	
+	private final static String API_KEY="54ab3a38257c69510bee581ee8a9c35b12eae44fd67042410283f05271f8d873";
+	private final static String SECRET_KEY="b10099101b6be660e8ded24bbef7d819b4266fce92b0f95c25ba917bc12874c4";
 
 	@ResponseBody
 	@RequestMapping(value = "/post/music")
@@ -23,7 +29,10 @@ public class MobileRequestController {
 		
 		Album alb = new URLHtml(artist).result;
 		
-		
+		App42API.initialize(API_KEY, SECRET_KEY);
+		PushNotificationService pushNotificationService = App42API.buildPushNotificationService();    
+		PushNotification pushNotification = pushNotificationService.sendPushMessageToUser("pappu","new Album released "+alb.getAlbumTitle());
+		System.out.println("hello");
 		return "new Album released "+alb.getAlbumTitle();
 	}
 
